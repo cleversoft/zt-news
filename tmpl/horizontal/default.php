e3e0fda1ef9e00a715e30ef174f2120083ab06a1
khangvm53 <khangvm530@gmail.com>
2015-03-27 15:52:05 +0700

    reformat horizontal layout

--- tmpl/horizontal/default.php
+++ tmpl/horizontal/default.php
@@ -1,21 +1,20 @@
 <?php
 /**
  * ZT News
- * 
+ *
  * @package     Joomla
  * @subpackage  Module
  * @version     2.0.0
- * @author      ZooTemplate 
- * @email       support@zootemplate.com 
- * @link        http://www.zootemplate.com 
+ * @author      ZooTemplate
+ * @email       support@zootemplate.com
+ * @link        http://www.zootemplate.com
  * @copyright   Copyright (c) 2015 ZooTemplate
  * @license     GPL v2
  */
 defined('_JEXEC') or die('Restricted access');
 
 $doc = JFactory::getDocument();
-if (!version_compare(JVERSION, '3.0', 'ge'))
-{
+if (!version_compare(JVERSION, '3.0', 'ge')) {
     $doc->addScript(JUri::root() . 'modules/mod_zt_news/assets/js/jquery-1.11.1.min.js');
 }
 $doc->addScript(JUri::root() . 'modules/mod_zt_news/assets/js/owl_carousel/owl.carousel.min.js');
@@ -23,18 +22,16 @@ $doc->addStyleSheet(JUri::root() . 'modules/mod_zt_news/assets/css/owl_carousel/
 $doc->addStyleSheet(JUri::root() . 'modules/mod_zt_news/assets/css/owl_carousel/owl.theme.css');
 $doc->addStyleSheet(JUri::root() . 'modules/mod_zt_news/assets/css/owl_carousel/owl.transitions.css');
 ?>
-<div class="zt_news_wrap"> 
+<div class="zt_news_wrap">
     <div class="zt-newsiv-frame-cat">
         <div id="zt-news-scroll-<?php echo $module->id; ?>" class="zt-category">
-            <?php
-            foreach ($items as $key => $item) :
-
-                ?>
+            <?php foreach ($items as $key => $item) : ?>
                 <div class="item zt-article-item">
                     <?php if (@$item->thumb != '' && $isImage == 1): ?>
                         <a href="<?php echo $item->link; ?>" title="<?php echo $item->title; ?>">
                             <img alt="<?php echo $item->title; ?>"
-                                 title="<?php echo $item->title; ?>" class="lazyOwl" data-src="<?php echo $item->thumb; ?>"/>
+                                 title="<?php echo $item->title; ?>" class="lazyOwl"
+                                 data-src="<?php echo $item->thumb; ?>"/>
                         </a>
                     <?php endif; ?>
                     <?php if ($showTitle): ?>
@@ -45,19 +42,22 @@ $doc->addStyleSheet(JUri::root() . 'modules/mod_zt_news/assets/css/owl_carousel/
                         </h3>
                     <?php endif; ?>
                     <?php if ($showCreated): ?>
-                        <span class="created"><?php echo JHTML::_('date', $item->created, JText::_('DATE_FORMAT_LC3')); ?> - <?php
+                        <span
+                            class="created"><?php echo JHTML::_('date', $item->created, JText::_('DATE_FORMAT_LC3')); ?>
+                            - <?php
                             echo $item->hits;
                             echo JText::_(' Views');
                             ?></span>
                     <?php endif; ?>
                     <?php if ($showIntro): ?>
-                        <?php  if ($item->introtext != false): ?>
-                            <p class="zt-introtext"><?php echo ($item->introtext); ?></p>
+                        <?php if ($item->introtext != false): ?>
+                            <p class="zt-introtext"><?php echo($item->introtext); ?></p>
                         <?php endif; ?>
                     <?php endif; ?>
-                    <?php if ($params->get('show_readmore') == 1): ?>
+                    <?php if ($showReadmore == 1): ?>
                         <p class="zt-news-readmore">
-                            <a class="readmore" href="<?php echo $item->link; ?>"><?php echo JTEXT::_('READ MORE'); ?></a>
+                            <a class="readmore"
+                               href="<?php echo $item->link; ?>"><?php echo JTEXT::_('READ_MORE'); ?></a>
                         </p>
                     <?php endif; ?>
                 </div>
@@ -74,6 +74,6 @@ $doc->addStyleSheet(JUri::root() . 'modules/mod_zt_news/assets/css/owl_carousel/
                 });
             });
         </script>
-    </div>   
+    </div>
     <div class="clearfix"></div>
 </div>
